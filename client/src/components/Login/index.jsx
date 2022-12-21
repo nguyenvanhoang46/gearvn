7950300fa6a3972b72ce9d58330521d7e435b454
nguyenvanhoang46 <nguyenvanhoang4601@gmail.com>
2022-12-21 22:58:19 +0700

    validate form

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
+import { useForm } from "react-hook-form";
 import { MdEmail } from 'react-icons/md'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup'
 import { RiLockPasswordFill } from 'react-icons/ri'
 import { useDispatch } from 'react-redux'
 import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai'
@@ -8,38 +11,32 @@ import { postLoginApi } from '../../app/services/authService'
 import Category from '../catergory'
 import Footer from '../Footer'
 import Header from '../Header'
-import { SubmitHandler, useForm } from 'react-hook-form'
+
+
+
+const schema = yup.object().shape({
+  email: yup.string("").required(),
+  password: yup.string().min(4).max(20).required(),
+});
 
 
 const Login = () => {
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-  const [error, seterror] = useState("");
-
-  const [passwordEye, setPasswordEye] = useState(false);
-
-  const handlePasswordClick = () => {
-    setPasswordEye(!passwordEye)
-  }
-
   
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const navigate = useNavigate();
+  const [hiddenPassword, setHiddenPassword] = useState(false)
+  const [errorsState, setErrorsState] = useState([])
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    if (!email || !password) return;
-    try {
-      const { accessToken, refreshToken } = await postLoginApi({ email, password });
-      localStorage.setItem("token", accessToken)
-      localStorage.setItem("refreshToken", refreshToken)
-      navigate("/");
-    } catch (e) {
-      seterror("Email or password invalid")
-      console.log(seterror);
-    }
+  const handleLogin = async (data) => {
+    data.preventDefault();
+    const response = await postLoginApi(data);
+    localStorage.setItem("token", response.accessToken)
+    localStorage.setItem("refreshToken", response.refreshToken)
+    navigate("/");
+
   }
-
   return (
     <>
       <div className='bg-[#f1f0f1]'>
@@ -49,23 +46,32 @@ const Login = () => {
       <div className="container mx-auto text-center mt-10">
         <h1 className='text-[36px] font-medium '>Đăng nhập</h1>
         <div className="mt-5 flex text-center justify-center">
-          <form onSubmit={handleLogin} method="">
-            <div className="flex">
-              <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
-                <MdEmail />
-              </span>
-              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className='w-[270px] py-[17px] px-[15px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Email' />
-            </div>
-            <div className="flex mt-5 relative ">
-              <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
-                <RiLockPasswordFill />
-              </span>
-              <input type={(passwordEye === false) ? 'password' : 'text'} value={password} onChange={(e) => setPassword(e.target.value)} className='w-[270px] py-[17px] px-[15px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Password' />
-              <div className="text-[20px] absolute top-2 right-2">
-                {
-                  (passwordEye === false) ? <AiFillEyeInvisible onClick={handlePasswordClick} /> : <AiFillEye onClick={handlePasswordClick} />
-                }
+          <form onSubmit={handleSubmit(handleLogin)}>
+            <div className="">
+              <div className="flex">
+                <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
+                  <MdEmail />
+                </span>
+                <input {...register("email")} className='w-[270px] py-[17px] px-[15px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Email' />
               </div>
+              <p className="ml-2 text-left text-[14px] text-red-900 ">{errors.email?.message}</p>
+            </div>
+            <div className="mt-5">
+              <div className="flex relative ">
+                <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
+                  <RiLockPasswordFill />
+                </span>
+                <input 
+                type={hiddenPassword ? "text" : "password"}
+                {...register("password")} 
+                className='w-[270px] py-[17px] px-[15px] text-[14px] text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Password' />
+                <div className="text-[20px] absolute top-2 right-2">
+                  {
+                    !hiddenPassword ? <AiFillEyeInvisible onClick={() => setHiddenPassword(e => !e)} /> : < AiFillEye onClick={() => setHiddenPassword(e => !e)} />
+                  }
+                </div>
+              </div>
+              <p className="ml-2 text-left text-[14px] text-red-900 ">{errors.password?.message}</p>
             </div>
             <div className="mt-7">
               <button type="submit" className='hover:bg-[black] bg-[#F0F0F0] uppercase hover:text-[white] cursor-pointer py-[7px] px-[14px] text-[14px] font-normal border-[1px] rounded-[4px] ' value="Đăng nhập" > Đăng nhập 		</button>
--- client/src/components/Register/index.jsx
+++ client/src/components/Register/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup'
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { BsTelephoneFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
@@ -11,47 +13,34 @@ import Category from '../catergory';
 import Footer from '../Footer';
 import Header from '../Header';
 
+const schema = yup.object().shape({
+  username: yup.string("").required(),
+  lastName: yup.string().required(),
+  firstName: yup.string().required(),
+  email: yup.string().email().required(),
+  phoneNumber: yup.string().min(10).required("Số điện thoại phải có ít nhất 10 ký tự"),
+  password: yup.string().min(4).max(20).required(),
+  confirmPassword: yup.string().oneOf([yup.ref("password"), null]),
+});
 
 const Register = () => {
-
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-
-  const onSubmit = data => alert(JSON.stringify(data));
-
-  const [username, setUserName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [password, setPassword] = useState("");
-  const [errros, setErrros] = useState([]);
-
-  const [confirmPassword, setConfirmPassword] = useState("");
-
-  const [passwordEye, setPasswordEye] = useState(false);
-  const [confirmPasswordEye, setConfirmPasswordEye] = useState(false);
-
-  const handlePasswordClick = (e) => {
-    e.preventDefault();
-    setPasswordEye(!passwordEye)
-  }
-  const handleConfirmPasswordClick = (e) => {
-    e.preventDefault();
-    setConfirmPasswordEye(!confirmPasswordEye)
-  }
-
   const navigate = useNavigate();
+  const [hiddenPassword, setHiddenPassword] = useState(false)
+  const [hiddenConfirmPassword, setHiddenConfirmPassword] = useState(false)
+  const [errorsState, setErrorsState] = useState([])
 
-  const handleRegisrer = async (e) => {
-    e.preventDefault();
-    const data = await postRegisterApi({ username, firstName, lastName, email, phoneNumber, password, confirmPassword });
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleRegisrer = async (data) => {
+    data.preventDefault();
     // console.log(data);
-    console.log(data.statusCode === 400)
-    if (data.statusCode === 400) {
-      console.log(data.errors);
-      setErrros(e => [...e, ...data.errors])
+    const response = await postRegisterApi(data);
+    if (response.statusCode !== 400) {
+      navigate("/login")
     } else {
-      navigate("/login");
+      setErrorsState(e => [...e, ...response.errors])
     }
   }
 
@@ -65,80 +54,93 @@ const Register = () => {
       <div className="container mx-auto text-center mt-10">
         <h1 className='text-[36px] font-medium '>Tạo tài khoản</h1>
         <div className="mt-5 flex text-center justify-center">
-          <form onSubmit={handleRegisrer} method="">
-            <div className="flex mt-5">
-              <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
-                <FaUser />
-              </span>
-              <input value={username} onChange={(e) => setUserName(e.target.value)} type="text" className='w-[230px] py-[17px] px-[12px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='UserName' />
-            </div>
-            <div className="flex mt-5">
-              <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
-                <FaUser />
-              </span>
-              <input value={firstName} onChange={(e) => setFirstName(e.target.value)} type="text" className='w-[230px] py-[17px] px-[12px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='First Name' />
-
-            </div>
-            <div className="flex mt-5">
-              <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
-                <FaUser />
-              </span>
-              <input value={lastName} onChange={(e) => setLastName(e.target.value)} type="text" className='w-[230px] py-[17px] px-[12px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Last Name' />
-            </div>
-            <div className="flex mt-5">
-              <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
-                <MdEmail />
-              </span>
-              <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className='w-[230px] py-[17px] px-[15px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Email' />
-
-            </div>
-            <div className="flex  mt-5">
-              <span className='py-[10px] px-[15px] text-[14px] text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
-                <BsTelephoneFill />
-              </span>
-              <input value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} type="text" className='w-[230px] py-[17px] px-[15px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Phone' />
-            </div>
-            <div className="flex mt-5 relative">
-              <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
-                <RiLockPasswordFill />
-              </span>
-              <input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                type={(passwordEye === false) ? 'password' : 'text'}
-              
-                className='w-[230px] py-[17px] px-[15px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Password' />
-              
-              <div className="text-[20px] absolute top-2 right-2">
-                {
-                  (passwordEye === false) ? <AiFillEyeInvisible onClick={handlePasswordClick} /> : <AiFillEye onClick={handlePasswordClick} />
-                }
-
-
+          <form onSubmit={handleSubmit(handleRegisrer)}>
+            <div className="mt-5">
+              <div className="flex">
+                <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
+                  <FaUser />
+                </span>
+                <input {...register("username")} type="text" className='w-[250px] py-[17px] px-[12px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='UserName' />
               </div>
+              <p className="ml-2 text-left text-[14px] text-red-900 ">{errors.username?.message}</p>
             </div>
-            <div className="flex  mt-5 relative">
-              <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
-                <RiLockPasswordFill />
-              </span>
-              <input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type={(confirmPasswordEye === false) ? 'password' : 'text'} className='w-[230px] py-[17px] px-[15px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Confirm Password' />
-              <div className="text-[20px] absolute top-2 right-2">
-                {
-                  (confirmPasswordEye === false) ? <AiFillEyeInvisible onClick={handleConfirmPasswordClick} /> : <AiFillEye onClick={handleConfirmPasswordClick} />
-                }
+            <div className="mt-5">
+              <div className="flex">
+                <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
+                  <FaUser />
+                </span>
+                <input {...register("lastName")} type="text" className='w-[250px] py-[17px] px-[12px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='First Name' />
               </div>
+              <p className="ml-2 text-left text-[14px] text-red-900 ">{errors.firstName?.message}</p>
             </div>
-            <div>{errros.map((item, index) => {
+            <div className="mt-5">
+              <div className="flex">
+                <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
+                  <FaUser />
+                </span>
+                <input {...register("firstName")} type="text" className='w-[250px] py-[17px] px-[12px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Last Name' />
+              </div>
+              <p className="ml-2 text-left text-[14px] text-red-900 ">{errors.lastName?.message}</p>
+            </div>
+            <div className=" mt-5">
+              <div className="flex">
+                <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
+                  <MdEmail />
+                </span>
+                <input {...register("email")} type="email" className='w-[250px] py-[17px] px-[15px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Email' />
+              </div>
+              <p className="ml-2 text-left text-[14px] text-red-900 ">{errors.email?.message}</p>
+            </div>
+            <div className="mt-5">
+              <div className="flex">
+                <span className='py-[10px] px-[15px] text-[14px] text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
+                  <BsTelephoneFill />
+                </span>
+                <input {...register("phoneNumber")} type="text" className='w-[250px] py-[17px] px-[15px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Phone' />
+              </div>
+              <p className="ml-2 text-left text-[14px] text-red-900 ">{errors.phoneNumber?.message}</p>
+            </div>
+            <div className="mt-5">
+              <div className="flex relative">
+                <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
+                  <RiLockPasswordFill />
+                </span>
+                <input
+                  type={hiddenPassword ? "text" : "password"}
+                  {...register("password")}
+                  className='w-[250px] py-[17px] px-[15px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Password' />
+                <div className="text-[20px] absolute top-2 right-2">
+                  {
+                    !hiddenPassword ? <AiFillEyeInvisible onClick={() => setHiddenPassword(e => !e)} /> : < AiFillEye onClick={() => setHiddenPassword(e => !e)} />
+                  }
+                </div>
+              </div>
+              <p className="ml-2 text-left text-[14px] text-red-900 ">{errors.password?.message}</p>
+            </div>
+            <div className="mt-5 ">
+              <div className="flex relative">
+                <span className='py-[10px] px-[15px] text-[14px]  text-[#555] bg-[#eee] border-[#ccc] rounded border-[1px] rounded-tr-[0px] rounded-br-[0px] border-r-0 border-t[0px] '>
+                  <RiLockPasswordFill />
+                </span>
+                <input type={hiddenConfirmPassword ? "text" : "password"} {...register("confirmPassword")} className='w-[250px] py-[17px] px-[15px] text-[14px]  text-[#555] border-[#ccc] rounded-tl-[0px] rounded-bl-[0px] focus:outline rounded border-[1px] h-[34px] focus:shadow focus:outline-[#66afe9]  ' placeholder='Confirm Password' />
+                <div className="text-[20px] absolute top-2 right-2">
+                  {
+                    !hiddenConfirmPassword ? <AiFillEyeInvisible onClick={() => setHiddenConfirmPassword(e => !e)} /> : < AiFillEye onClick={() => setHiddenConfirmPassword(e => !e)} />
+                  }
+                </div>
+              </div>
+              <p className="ml-3 w-[290px] text-left text-[14px] text-red-900 ">{errors.confirmPassword?.message}</p>
+            </div>
+            <div>{errorsState.map((item, index) => {
               return (
                 <p key={index}>{item.message}</p>
               );
             })}</div>
             <div className="flex gap-8 justify-center mt-8">
-              <button type="submit" className='hover:bg-[black] bg-[#F0F0F0] uppercase hover:text-[white] cursor-pointer py-[7px] px-[14px] text-[14px] font-normal border-[1px] rounded-[4px] ' value="Đăng ký"> Đăng ký	</button>
+              <button type="submit" className='hover:bg-[black] bg-[#F0F0F0] uppercase hover:text-[white] cursor-pointer py-[7px] px-[14px] text-[14px] font-normal border-[1px] rounded-[4px] ' > Đăng ký	</button>
               <div className="hover:bg-[black]  uppercase hover:text-[white] py-[7px] px-[14px] rounded-[5px] bg-[#ccc] ">
                 <Link to='/' className="">Quay về</Link>
               </div>
-
             </div>
 
           </form>
