1205f12410353a75a42783d0eb5e9dc7c79afb94
NTAK666 <=>
2022-12-30 22:59:29 +0700

    app(api): controller odering

--- api/api/api/MapperProfiles/OrderDetailProfile.cs
+++ api/api/api/MapperProfiles/OrderDetailProfile.cs
@@ -6,16 +6,12 @@ namespace api.MapperProfiles;
 
 public class OrderDetailProfile : Profile
 {
-  private readonly IMapper _mapper;
-
-  public OrderDetailProfile(IMapper mapper)
+  public OrderDetailProfile()
   {
-    _mapper = mapper;
-
     CreateMap<OrderDetail, OrderDetailDto>()
       .ForMember(dest => dest.Product, opt => opt.MapFrom(src => src.Product))
       .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity))
-      .ForMember(dest => dest.Order, opt => opt.MapFrom(src => _mapper.Map<Order, OrderDto>(src.Order)))
+      .ForMember(dest => dest.Order, opt => opt.MapFrom(src => src.Order))
       .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
       .ReverseMap();
   }
